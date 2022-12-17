f5e4a6ab02b77396ee07279345cc229b25efebed
miclegoden <darklerbrian1025@gmail.com>
2022-12-18 02:19:27 +0900

    jobs UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Header from "../components/Header";
-import Landing_lady from "../components/Landing-lady-thing";
-import LandingContainer from "../components/LandingContainer";
+import Header from '../components/Header'
+import Landing_lady from '../components/Landing-lady-thing'
+import LandingContainer from '../components/LandingContainer'
+import Landing_JobStyle from '../components/Landing-job-style'
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-     <Header/>
-     <Landing_lady/>
-
-
-     <LandingContainer/>
+      <Header />
+      <Landing_lady />
+      <Landing_JobStyle />
+      <LandingContainer />
     </div>
   )
 }
